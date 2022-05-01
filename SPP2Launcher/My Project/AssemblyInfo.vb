9cde89ffae1c5dc97b6521481bf6fd816a4047b3
SPP User <spp-user@spp-forum.de>
2022-05-01 22:49:01 +0600

    Изменение версии

--- SPP2Launcher/My Project/AssemblyInfo.vb
+++ SPP2Launcher/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' используя "*", как показано ниже:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("0.152.4.237")>
-<Assembly: AssemblyFileVersion("0.152.4.237")>
+<Assembly: AssemblyVersion("0.158.1.12")>
+<Assembly: AssemblyFileVersion("0.158.1.12")>
